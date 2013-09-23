b268f870cd3ed3da17d1dc662b0b9bf6c5a8f0c9
vagoum <bgoumas@gmail.com>
2013-09-23 20:47:44 +0300

    Input for keylength and Miller-Rabin parameter added

--- rsa.py
+++ rsa.py
@@ -1,7 +1,9 @@
-import random,sys
+#! /usr/local/bin/python
+import random
+import sys
 
 
-def Miller_Rabin(n,k):
+def Miller_Rabin(n, k=100):
     if n % 2 == 0:
         return False
     s = 0
@@ -11,8 +13,8 @@ def Miller_Rabin(n,k):
         s += 1
     assert(2**s*d == n-1)
     for i in xrange(k):
-        a = random.randrange(2,n)
-        return validation(a,d,n,s)
+        a = random.randrange(2, n)
+        return validation(a, d, n, s)
         if not validation:
             return False
     return True
@@ -32,8 +34,8 @@ def extended_gcd(a,b):
     aux2 = 1
     n = a
     while b != 0:
-        q = a/b
-        r = a%b
+        q = a / b
+        r = a % b
         a = b
         b = r
         temp = aux2
@@ -49,43 +51,41 @@ def gcd(m,n):
     	n = m%n
     	m = temp
     return m
-    
-    
-p = [0, 0]
-for i in range(0, 2):    
-    p[i] = random.getrandbits(int(26))
-    while Miller_Rabin(p[i],100) != True:
-        p[i] = random.getrandbits(26)
-modulus = p[0]*p[1]
-totient = (p[0]-1)*(p[1]-1)
-for i in range(2,int(totient**0.5)): 
-    if gcd(totient, i) == 1:
-        public_enc_key = i
-        break
-private_dec_key = extended_gcd(totient,
-                               public_enc_key)
 
-print modulus
-print public_enc_key
-print private_dec_key
-#Initilization of the encryption/decryption process
-
-user_input = ">>>"
-plaintext = int(raw_input(user_input))
-print "Enter numeral for encryption:"
-while plaintext > modulus:
-    print "Invalid message length.Please try a shorter numeral"
-    print "Enter numeral for encryption:"
+def get_comprime(number):
+    for i in range(2,int(number**0.5)):
+        if gcd(number, i) == 1:
+        	   return i 
+        	     
+def init_rsa(bitlength, millerab_par):    
+    p = [0, 0]
+    for i in range(0, 2):    
+        p[i] = random.getrandbits(int(bitlength))
+        while Miller_Rabin(p[i],millerab_par) != True:
+            p[i] = random.getrandbits(bitlength)
+    modulus = p[0]*p[1]
+    totient = (p[0]-1)*(p[1]-1)
+    public_enc_key = get_comprime(totient)
+    private_dec_key = extended_gcd(totient,
+                                   public_enc_key)
+    return modulus,public_enc_key,private_dec_key
+                               
+if __name__ == "__main__":
+    import sys
+    pseudoprime, public_key, private_key = init_rsa(int(sys.argv[1]), int(sys.argv[2]))
+    print pseudoprime
+    print public_key
+    print private_key
+    user_input = ">>>"
     plaintext = int(raw_input(user_input))
-ciphertext = pow(plaintext,
-                 public_enc_key, modulus)
-print "The cipher text is :%d" %ciphertext
-decode = pow(ciphertext, 
-             private_dec_key, modulus)
-print "Decoded text: %d" %decode
-
-
-
-
-       
-    
+    print "Enter numeral for encryption:"
+    while plaintext > pseudoprime:
+        print "Invalid message length.Please try a shorter numeral"
+        print "Enter numeral for encryption:"
+        plaintext = int(raw_input(user_input))
+    ciphertext = pow(plaintext,
+                     public_key, pseudoprime)
+    print "The cipher text is :%d" %ciphertext
+    decode = pow(ciphertext, 
+             private_key, pseudoprime)
+    print "Decoded text: %d" %decode
